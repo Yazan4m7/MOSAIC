46318ff15f7260a190affa5d03fceb0736e99945
Yazan <yazan4m7@hotmail.com>
2020-01-12 15:54:32 +0200

    NOthing

---
+++ lib/Employee.dart
@@ -0,0 +1,17 @@
+class Employee {
+  int id;
+  String firstName;
+  String lastName;
+
+  Employee({this.id,this.firstName,this.lastName});
+
+  factory Employee.fromJson(Map<String,dynamic>json){
+  Employee emp= Employee(
+    id: json['id'] as int,
+    firstName:json['first_name'] as String,
+    lastName:json['last_name'] as String,
+  );
+  print ("emp to string " + emp.toString());
+  return emp;
+  }
+}
