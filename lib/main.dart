dca11dd6c343b4dcce02bc553c1b0a6a7feafad8
Yazan <yazan4m7@hotmail.com>
2020-02-17 17:33:29 +0200

    Works

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_app3/views/Screens.dart';
 import 'package:flutter_app3/views/LoginUI.dart';
 
-
+import 'SignIn1.dart';
 
 void main() => runApp(MaterialApp(
-home:new Scaffold(body: new LoginUI())));
+home:new Scaffold(body: new SignInOne())));
